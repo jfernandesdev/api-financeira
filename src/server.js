0f23463f22c1aca26f0aab43bf3698663de91502
jfernandesdev <jfernandes.dev@gmail.com>
2023-02-12 17:48:17 -0300

    FEAT: routes to get user account data and update data

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { response } from 'express'
 import { randomUUID } from 'node:crypto'
 
 const app = express()
@@ -113,6 +113,23 @@ app.get('/statement/date', verifyIfExistsAccountCPF, (request, response) => {
   )
 
   return response.json(statement)
+})
+
+app.get('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
+
+  return response.json(customer)
+})
+
+
+app.put('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body
+  const { customer } = request
+
+  customer.name = name;
+
+  return response.status(201).send()
+
 })
 
 app.listen(3333)
