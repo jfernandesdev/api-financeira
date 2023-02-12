fc92cd8bb22a29399bdb8cacc5dd5e0b9d7fc43f
jfernandesdev <jfernandes.dev@gmail.com>
2023-02-12 11:07:19 -0300

    FEAT: route create new account

--- src/server.js
+++ src/server.js
@@ -1,9 +1,32 @@
 import express from 'express'
+import { randomUUID } from 'node:crypto'
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!' })
+const customers = []
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  )
+
+  if(customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists!'})
+  }
+
+  const newAccount = {
+    id: randomUUID(),
+    cpf,
+    name,
+    statement: []
+  }
+
+  customers.push(newAccount)
+
+  return response.status(201).send()
 })
 
 app.listen(3333)
